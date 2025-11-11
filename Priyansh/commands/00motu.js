e706d8f2e29a75205b93df99154c306af79beeb5
bhabhisingh8906-web <bhabhisingh8906@gmail.com>
2025-11-11 13:53:46 +0530

    Update 00motu.js

--- Priyansh/commands/00motu.js
+++ Priyansh/commands/00motu.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-    var link = ["https://i.imgur.com/n0Q1VHO.mp4" ];
+    var link = ["https://i.imgur.com/n0Q1VHO.mp4","https://i.imgur.com/2Bey6es.mp4" ];
      var callback = () => api.sendMessage({body:`Motu patlu Funny😹`,attachment: fs.createReadStream(__dirname + "/cache/1.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.mp4"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.mp4")).on("close",() => callback());
    };
