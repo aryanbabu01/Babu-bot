7886a94cd253a43f2a928425ab29c23777261de0
bhabhisingh8906-web <bhabhisingh8906@gmail.com>
2025-11-15 19:17:54 +0530

    Update and rename 00nach b.js to 00ranu.js

--- Priyansh/commands/00ranu.js
+++ Priyansh/commands/00ranu.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "nach",
+  name: "ranu",
     version: "2.1.1",
   hasPermssion: 0,
   credits: "PRINCE RAJPUT", 
@@ -13,15 +13,15 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var name = await Users.getNameUser(event.senderID);
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("nach") ||
-     react.includes("nachna") ||
-     react.includes("nch") ||
-react.includes("nachh")) {
+  if(react.includes("ranu") ||
+     react.includes("mandal") ||
+     react.includes("RanU") ||
+react.includes("MANDAL")) {
     var msg = {
-        body: `🙈😒😒👈`,attachment: fs.createReadStream(__dirname + `/noprefix/basanti.mp4`)
+        body: `🙈😒😒👈`,attachment: fs.createReadStream(__dirname + `/noprefix/ranu.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("💃", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🤱", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
